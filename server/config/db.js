1f5d60653e1d54fa1a090806decfb7ee37c48afd
mbhadani001 <105386899+mbhadani001@users.noreply.github.com>
2022-08-18 21:39:39 -0400

    db connected

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    "URI": "mongodb://127.0.0.1:27017/surveys",
+    "URI": "mongodb+srv://efrenalb7:SNOHVylW2QORW3h8@mongodbserver.we4fsno.mongodb.net/superSurvey?retryWrites=true&w=majority",
        "Secret": 'SomeSecret'
 
 }
